4ee12c6743ba4b47294138f26e240149292458ce
vanshik0027 <vanshik0027@gmail.com>
2024-04-23 12:49:03 +0530

    add aws key variable

--- variables.tf
+++ variables.tf
@@ -1,3 +1,15 @@
+variable "aws_access_key" {
+  description = "AWS access key"
+  type = string
+  default = ""
+}
+
+variable "aws_secret_key" {
+  description = "aws secret key"
+  type = string
+  default = ""
+}
+
 variable "ports" {
   type = list(number)
 
